c037274343123aeb254939ed10cccba8d2430de9
ManasParauha <manashparauha@gmail.com>
2024-04-04 00:09:52 +0530

    debugging of video detail fetch

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -38,8 +38,8 @@ const HeroSection = () => {
 
 
       <div className='absolute top-20 sm:top-9 md:top-36 left-7 flex flex-col justify-center text-foreground w-[80%] sm:w-[60%] md:w-[50%] gap-4'>
-        <h6 className=' font-bold text-4xl sm:text-5xl '>Demon Slayer</h6>
-        <p className='font-semibold h-16 w-full md:h-20 text-ellipsis  overflow-hidden text-sm text-justify sm:block hidden'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptas magni odio adipisci quos, in doloribus sapiente voluptate cum quidem reiciendis officia incidunt dolore dicta odit? Quia ratione quasi nam est nihil!
+        <h6 className=' font-bold text-4xl sm:text-5xl '>{title}</h6>
+        <p className='font-semibold h-16 w-full md:h-20 text-ellipsis  overflow-hidden text-sm text-justify sm:block hidden'> {description}
         </p>
         <div className='flex  gap-2 w-full'>
         <Button><PlayArrowIcon className="mr-2 h-4 w-4"/>Play</Button>
