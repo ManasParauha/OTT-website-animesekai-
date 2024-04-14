e69528f91579f0fa88d24ae307743d2a1401e3ea
ManasParauha <manashparauha@gmail.com>
2024-04-14 19:02:30 +0530

    bug fixes

--- src/components/TopMovies.tsx
+++ src/components/TopMovies.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import poster from "../../public/Naruto.jpg"
+import poster from "../../public/blur.jpg"
 import { Button } from "@/components/ui/button"
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
@@ -16,63 +16,63 @@ const TopMovies = () => {
 
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
       </div>
       <div className='group relative flex justify-center h-40 w-60  '>
         <Image src={poster} alt='#' className='min-h-40 min-w-60  ' />
-        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '>Naruto</p>
+        <p className='group-hover:opacity-100 opacity-0  absolute top-3 text-2xl font-semibold text-foreground transition-opacity  '></p>
         <Button className=' transition-opacity gap-2 group-hover:opacity-100 opacity-0 flex absolute bottom-3 z-20'><PlayArrowIcon/>Play Now</Button>
         <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition duration-300"></div>
 
