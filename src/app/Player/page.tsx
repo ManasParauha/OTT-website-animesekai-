915ec89260752ac157b2f52d2080b157077882b7
ManasParauha <manashparauha@gmail.com>
2024-04-14 19:46:47 +0530

    created a player route

--- src/app/Player/page.tsx
+++ src/app/Player/page.tsx
@@ -1,11 +1,43 @@
+"use client"
 import React from 'react'
+import NavbarMD from "@/components/NavbarMD";
+import Navbar from "@/components/Navbar";
+import LogoUser from "@/components/LogoUser";
+import Image from 'next/image';
+import poster from '../../../public/blur.jpg'
+
 
 const page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='flex bg-background'>
+
+            <div className="fixed hidden md:block left-0 top-0 z-10"><NavbarMD /></div>
+            <div className="w-full relative md:ml-32 flex flex-col overflow-x-hidden items-center ">
+                <LogoUser />
+                
+                <video controls src="https://files.edgestore.dev/8ntc2l4n0afyxvtk/publicFiles/_public/81febc2c-dd14-41cc-9687-9667061ff3d2.mp4" className=' object-fill w-full h-[40vh]  sm:h-[50vh] md:w-[80vw] md:h-[70vh]  md:mt-10 sm:mt-16 mt-20 '></video>
+
+                <div className='flex gap-2 mt-3 w-full md:ml-28'>
+                    <Image src={poster} alt='Thumbnail' className='h-20 w-28'/>
+                    <div className='flex gap-1 flex-col '>
+                        <h6 className='text-foreground text-2xl font-semibold'>Heading</h6>
+                        <p className='text-muted-foreground text-sm'>Episode - 1</p>
+                    </div>
+
+                </div>
+
+
+                <Navbar />
+                <div className="h-20 md:h-0"></div>
+
+
+
+
+            </div>
+
+
+        </div>
+    )
 }
 
 export default page
