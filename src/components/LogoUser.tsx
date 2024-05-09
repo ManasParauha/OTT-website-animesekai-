5b3a186515bd177acdcd6d093a0a37ead7e766ea
ManasParauha <manashparauha@gmail.com>
2024-05-09 15:01:37 +0530

    created a button for dark and light mode

--- src/components/LogoUser.tsx
+++ src/components/LogoUser.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import Image from 'next/image'
 import Logo from "../../public/logo.png"
 import { Button } from "@/components/ui/button"
@@ -20,8 +20,19 @@ import {
 } from "@/components/ui/sheet"
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const LogoUser = () => {
+import * as React from "react"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+ 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
+const LogoUser = () => {
+    const { setTheme } = useTheme()
     const router = useRouter()
     
     const [data,setData] = React.useState("Username");
@@ -52,6 +63,32 @@ const LogoUser = () => {
   return (
      <header className='md:hidden bg-background w-full h-16 flex  items-center p-5 justify-between'>
         <Image src={Logo} width={50} height={15} alt='Logo'/>
+
+
+        <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+
+
+
+
         
         <div className='flex gap-2'>
         
